5924e4d5ca5790a498bcafcb8acaa741fd00b7b6
DevThomaz <socialdevguithz@gmail.com>
2024-12-12 09:58:51 -0300

    :sparkles: feat:Database connected

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,22 @@
-const create = (req, res) => {
+const userService = require('../services/user.service')
+const mongoose = require("mongoose");
+
+const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
 
     if(!name|| !username || !email || !password || !avatar || !background){
         res.status(400).send({message:"Submit all form filds for registration"})
     }
     
+    const user = await userService.createService(req.body);
+    if (!user){
+        return res.status(400).send({message: "error creating User"});
+    }
+
     res.status(201).send({
         message:"User create successfully",
         user: {
+            id: user._id,
             name,
             username,
             email,
@@ -17,5 +26,28 @@ const create = (req, res) => {
     })
 };
 
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
 
-module.exports = { create };
+    if(users.length === 0){
+        return res.statu(400).send({message:"There are no registered users"})
+    }
+    res.send(users)
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({message:"invalid ID"})
+    }
+
+    const user =  await userService.findByIdService(id)
+
+    if(!user){
+        return res.status(400).send({message:"User not found"})
+    }
+    res.send(user)
+}
+
+
+module.exports = { create, findAll, findById};
---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose'); 
+
+const connectDatabase = () => {
+    console.log("Wait, conecting to the Database")
+    
+    mongoose.connect(
+        "mongodb+srv://root:root@cluster0.vf0xd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        {useNewUrlParser: true,
+        useUnifiedTopology: true,}
+    )
+    .then(() => console.log("MongoDb Atlas connected"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
