3d7ab24957c10b17c94b15e07a7b1c4269b2cdc5
DevThomaz <socialdevguithz@gmail.com>
2024-12-13 09:19:51 -0300

    :recycle: Switching to imports

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
-const userService = require('../services/user.service')
-
+import userService from '../services/user.service.js';
 const create = async (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body;
+    try{
+        const {name, username, email, password, avatar, background} = req.body;
 
     if(!name|| !username || !email || !password || !avatar || !background){
         res.status(400).send({message:"Submit all form filds for registration"})
@@ -21,17 +21,22 @@ const create = async (req, res) => {
             email,
             avatar,
             background
-        }
+        },
     })
+}catch(err){
+    res.status(500).send({message:err.message})
+}
 };
 
 const findAll = async (req, res) => {
-    const users = await userService.findAllService();
+    try{const users = await userService.findAllService();
 
     if(users.length === 0){
         return res.statu(400).send({message:"There are no registered users"})
     }
-    res.send(users)
+    res.send(users)}catch(err){
+        res.status(500).send({message:err.message});
+    }
 };
 
 const findById = async (req, res) => {
@@ -63,4 +68,4 @@ const update = async (req, res) => {
     res.send({message:"user successfully update!"})
 };
 
-module.exports = { create, findAll, findById, update};
+export default { create, findAll, findById, update};
