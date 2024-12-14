52f1cdbcee53611a037b78b67892636d484bb487
DevThomaz <socialdevguithz@gmail.com>
2024-12-14 15:30:18 -0300

    feat: :sparkles: add route protection with JWT

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -4,6 +4,23 @@ import {ObjectId} from "mongoose";
 
 const create = async  (req, res) => {
     try{
+        const {authorization} = req.headers;
+        if(!authorization){
+            return res.send(401);
+        };
+
+        const parts = authorization.split(" ");
+
+        
+        if (parts.length !== 2){
+            return res.sendStatus(401)
+        }
+        const [schema, token] = parts;
+        
+        if (schema !== "Bearer"){
+            return res.sendStatus(401)
+        }
+
         const {title, text, banner} = req.body;
 
         if(!title||!banner||!text){
@@ -19,7 +36,7 @@ const create = async  (req, res) => {
             user: {_id:"675c57fd70da8e50894b4556"},
         });
 
-        res.send(201)
+        res.sendStatus(201)
     }catch(err){
         res.status(500).send({message: err.message});
     }    
@@ -28,7 +45,7 @@ const create = async  (req, res) => {
 const findAll = async (req, res) => {
     const news = await findAllService();
     if(news.length === 0){
-        return res.statu(400).send({message:"There are no registered news",})
+        return res.status(400).send({message:"There are no registered news",})
     };
     res.send(news);
 }
