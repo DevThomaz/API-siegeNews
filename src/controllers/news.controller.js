d51f5919fa926db27d4afd100e142e8b5d742a6c
DevThomaz <socialdevguithz@gmail.com>
2024-12-15 01:13:30 -0300

    feat: :sparkles: Add byUser

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,12 @@
-import {createService, findAllService, countNews, topNewsService, findByIdService} from "../services/news.service.js"
+import {
+    createService, 
+    findAllService, 
+    countNews, 
+    topNewsService, 
+    findByIdService,
+    searchByTitleService,
+    byUserService
+} from "../services/news.service.js"
 import {ObjectId} from "mongoose";
 
 
@@ -127,5 +135,60 @@ export const findById = async (req, res) => {
     }catch(err){
         res.status(500).send({message: err.message});
     };
-}
+};
 
+export const searchByTitle = async (req, res) => {
+    try{
+        const { title } = req.query;
+
+        const news = await searchByTitleService(title);
+
+        if(news.length === 0) {
+            return res
+            .status(400)
+            .send({message: "There are no news with this title"});
+        }
+
+        return res.send({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+    
+            })),
+        });
+
+    }catch(err){
+        res.status(500).send({message: err.message});
+    };
+};
+
+export const byUser = async(req, res) => {
+    try{
+        const id = req.userId;
+        const news = await byUserService(id);
+
+        return res.send({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+    
+            })),
+        });
+    }catch(err){
+        res.status(500).send({message: err.message});
+    };
+};
